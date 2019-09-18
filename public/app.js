861cc53ab5bf4df9e1e3a1085a4f0a1fc79f6611
tofik90 <ru-tofik@yandex.ru>
2019-09-18 19:27:27 +0300

    add function add_to_cart

--- public/app.js
+++ public/app.js
@@ -7,8 +7,30 @@ function something()
 		alert(x);
 	}
 
-function add_to_cart(id) 
-	{		
-		alert('you added pizza with id: ' + id);
+  
+function add_to_cart(id)
+{
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
+}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
 	}
 
+	return cnt;
+}
